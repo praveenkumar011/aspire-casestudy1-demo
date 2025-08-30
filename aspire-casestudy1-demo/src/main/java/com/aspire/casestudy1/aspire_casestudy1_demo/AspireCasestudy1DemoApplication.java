c153ee8776848195eebfa285066ac244c2c35b28
Praveen Kumar <praveenkumar011@gmail.com>
2025-08-30 19:01:55 +0530

    First Commit

---
+++ aspire-casestudy1-demo/src/main/java/com/aspire/casestudy1/aspire_casestudy1_demo/AspireCasestudy1DemoApplication.java
@@ -0,0 +1,13 @@
+package com.aspire.casestudy1.aspire_casestudy1_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspireCasestudy1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspireCasestudy1DemoApplication.class, args);
+	}
+
+}
---
+++ aspire-casestudy1-demo/src/test/java/com/aspire/casestudy1/aspire_casestudy1_demo/AspireCasestudy1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspire.casestudy1.aspire_casestudy1_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspireCasestudy1DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
